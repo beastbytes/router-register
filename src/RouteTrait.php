47ec6c5ea4554363731ac283292788868f60c326
Chris <chris@clyates.plus.com>
2025-06-06 13:38:14 +0100

    getName() returns name including group and prefix

--- src/RouteTrait.php
+++ src/RouteTrait.php
@@ -6,9 +6,11 @@ namespace BeastBytes\Router\Register;
 
 trait RouteTrait
 {
-    public function getName(): string
+    public function getName(?string $prefix = null, ?GroupInterface $group = null): string
     {
-        return $this->name;
+        $name = $group instanceof GroupInterface ? $group->getNamePrefix() : '';
+        $name .= $prefix !== null ? $prefix . '_' : '';
+        return $name . $this->name;
     }
 
     public function getUri(): string
