c1b013a60e106ee58f492daf70914042bd58298e
Chris <chris@clyates.plus.com>
2025-08-11 13:36:38 +0100

    Add GetPost.php convenience HTTP method attribute

---
+++ src/Attribute/Method/GetPost.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BeastBytes\Router\Register\Attribute\Method;
+
+use BeastBytes\Router\Register\Attribute\Route;
+use BeastBytes\Router\Register\RouteInterface;
+use Attribute;
+
+#[Attribute(Attribute::TARGET_METHOD)]
+final class GetPost extends Route
+{
+    public function __construct(RouteInterface $route)
+    {
+        parent::__construct([Method::GET, Method::POST], $route);
+    }
+}
