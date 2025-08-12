f9b0f189b046e5363248797aa9d68fbcc8aaae5c
Chris <chris@clyates.plus.com>
2025-08-12 23:23:55 +0100

    Simplify Route attribute

--- src/Attribute/Route.php
+++ src/Attribute/Route.php
@@ -11,31 +11,15 @@ use Attribute;
 #[Attribute(Attribute::TARGET_METHOD)]
 class Route implements RouteAttributeInterface
 {
-    /** @var list<callable|string> */
-    private array $disableMiddleware = [];
-    /** @var list<string> */
-    private array $hosts = [];
-    /** @var list<callable|string> */
-    private array $middleware = [];
-
     /**
      * @param list<Method> $methods
      * @param RouteInterface $route
-     * @param list<string> $hosts
-     * @param list<callable|string> $middleware
-     * @param list<callable|string> $disableMiddleware
      */
     public function __construct(
         private readonly array $methods,
         private readonly RouteInterface $route,
-        array|string $hosts = [],
-        array|callable|string $middleware = [],
-        array|callable|string $disableMiddleware = [],
     )
     {
-        $this->hosts = is_array($hosts) ? $hosts : [$hosts];
-        $this->middleware = is_array($middleware) ? $middleware : [$middleware];
-        $this->disableMiddleware = is_array($disableMiddleware) ? $disableMiddleware : [$disableMiddleware];
     }
 
     /**
@@ -50,30 +34,6 @@ class Route implements RouteAttributeInterface
         }
 
         return $methods;
-    }
-
-    /**
-     * @return list<string>
-     */
-    public function getHosts(): array
-    {
-        return $this->hosts;
-    }
-
-    /**
-     * @return list<callable|string>
-     */
-    public function getDisableMiddleware(): array
-    {
-        return $this->disableMiddleware;
-    }
-
-    /**
-     * @return list<callable|string>
-     */
-    public function getMiddleware(): array
-    {
-        return $this->middleware;
     }
 
     public function getName(): string
