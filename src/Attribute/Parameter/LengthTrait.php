4fd5965ea2b2acda0ec30340eaa94111618560d6
Chris <chris@clyates.plus.com>
2025-10-12 00:52:00 +0100

    Update LengthTrait.php

--- src/Attribute/Parameter/LengthTrait.php
+++ src/Attribute/Parameter/LengthTrait.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace BeastBytes\Router\Register\Attribute\Parameter;
 
+use RuntimeException;
+
 trait LengthTrait
 {
     private function getLength(array|int|null $length, bool $nonZero = false): string
@@ -13,14 +15,25 @@ trait LengthTrait
         }
 
         if (is_int($length)) {
-            if ($nonZero) {
-                return '{' . ($length - 1) . '}';
+            if ($length > 0) {
+                if ($nonZero) {
+                    return '{' . ($length - 1) . '}';
+                }
+
+                return '{' . $length . '}';
             }
 
-            return '{' . $length . '}';
+            throw new RuntimeException('Integer values for $length must be > 0');
         }
 
         [$min, $max] = $length;
+
+        if ((is_int($min) && $min < 0) || (is_int($min) && $max < 0)) {
+            throw new RuntimeException('Integer values for `min` and `max` must be >= 0');
+        }
+        if (is_int($min) && is_int($max) && $max <= $min) {
+            throw new RuntimeException('`max` must be > `min`');
+        }
 
         if ($nonZero) {
             if (is_int($min) && $min > 0) {
