e3ea747560e331cbcad724f2fd39584a62a5a277
Chris <chris@clyates.plus.com>
2025-11-09 16:46:27 +0000

    Refactoring

--- src/Attribute/Parameter/Alphanumeric.php
+++ src/Attribute/Parameter/Alphanumeric.php
@@ -7,7 +7,7 @@ namespace BeastBytes\Router\Register\Attribute\Parameter;
 use Attribute;
 
 #[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
-final class AlphaNumeric extends Pattern
+final class Alphanumeric extends Pattern
 {
     use LengthTrait;
 
